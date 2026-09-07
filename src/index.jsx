988ac4fc10314af6bd2b28195104c0153e19ce42
Ata Ur Rehman <ataurrehman56@gmail.com>
2026-09-07 15:50:03 +0500

    feat: remove category from studio

--- src/index.jsx
+++ src/index.jsx
@@ -29,7 +29,6 @@ import Head from './head/Head';
 import { StudioHome } from './studio-home';
 import { ProgramDetailPage } from './programs';
 import { InstructorDetailPage } from './instructors';
-import { CategoryDetailPage } from './categories';
 import CourseRerun from './course-rerun';
 import { TaxonomyLayout, TaxonomyDetailPage, TaxonomyListPage } from './taxonomy';
 import { ContentTagsDrawer } from './content-tags-drawer';
@@ -72,8 +71,6 @@ const App = () => {
         <Route path="/programs/:programId" element={<ProgramDetailPage />} />
         <Route path="/instructors" element={<StudioHome />} />
         <Route path="/instructors/:instructorId" element={<InstructorDetailPage />} />
-        <Route path="/categories" element={<StudioHome />} />
-        <Route path="/categories/:categoryId" element={<CategoryDetailPage />} />
         <Route path="/libraries" element={<StudioHome />} />
         <Route path="/libraries-v1" element={<StudioHome />} />
         <Route path="/libraries-v1/migrate" element={<LegacyLibMigrationPage />} />
